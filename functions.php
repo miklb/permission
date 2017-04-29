29d134faa0562ca860f1efebc00c9d9ca9c174b3
Michael Bishop <miklb@miklb.com>
2017-04-28 22:15:40 -0400

    fix: main content wrapper

--- functions.php
+++ functions.php
@@ -22,3 +22,17 @@ function permission_widgets_init() {
 
 }
 	add_action( 'widgets_init', 'permission_widgets_init' );
+
+	/**
+	 * Enqueue scripts.
+	 */
+	function permission_scripts() {
+		wp_enqueue_style( 'permission-style', get_template_directory_uri() . '/style.min.css' );
+		if (
+	        is_singular()
+	        and get_option( 'thread_comments' )
+	    )
+	        wp_enqueue_script( 'comment-reply' );
+}
+
+	add_action( 'wp_enqueue_scripts', 'permission_scripts' );
--- index.php
+++ index.php
@@ -6,12 +6,18 @@
  */
 
 get_header(); ?>
-  <div class="columns">
-    <div class="column is-three-quarters">
-      <p>Main column</p>
-    </div><!-- end main column-->
-    <div class="column">
-      <?php get_sidebar(); ?>
-    </div><!-- end sidebar -->
-  </div><!-- end columns -->
+	<div class="section" id="main">
+		<div class="container">
+			<div class="columns">
+				<div class="column is-three-quarters">
+					<p>Main section</p>
+				</div><!-- end main column-->
+				<div class="column">
+					<?php get_sidebar(); ?>
+				</div><!-- end sidebar -->
+			</div><!-- end columns -->
+		</div>
+	</div>
+
+
 <?php get_footer(); ?>
